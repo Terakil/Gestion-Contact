357b6d8b7d389a822f96c1e0213f0900324dac0f
Terakil <terakilarno@gmail.com>
2026-04-22 22:32:46 +0300

    ranger

---
+++ build/CMakeFiles/contact.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/contact.dir/link.d"
+  "CMakeFiles/contact.dir/main.c.o"
+  "CMakeFiles/contact.dir/main.c.o.d"
+  "CMakeFiles/contact.dir/src/Contact.c.o"
+  "CMakeFiles/contact.dir/src/Contact.c.o.d"
+  "CMakeFiles/contact.dir/src/Search.c.o"
+  "CMakeFiles/contact.dir/src/Search.c.o.d"
+  "CMakeFiles/contact.dir/src/Tri.c.o"
+  "CMakeFiles/contact.dir/src/Tri.c.o.d"
+  "contact"
+  "contact.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/contact.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
